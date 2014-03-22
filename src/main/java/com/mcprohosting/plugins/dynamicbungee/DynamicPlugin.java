915c2d3371507b56ea480a016ac05e98d4677caf
Evan Lindsay <evanlindsay@live.com>
2014-03-21 20:28:44 -0700

    Plugin data folder

--- src/main/java/com/mcprohosting/plugins/dynamicbungee/DynamicPlugin.java
+++ src/main/java/com/mcprohosting/plugins/dynamicbungee/DynamicPlugin.java
@@ -1,13 +1,26 @@
 package com.mcprohosting.plugins.dynamicbungee;
 
+import java.io.File;
+
 public abstract class DynamicPlugin {
 
     private final String NAME = this.getClass().getSimpleName();
+    private final File FOLDER = new File(DynamicBungee.getPlugin().getDataFolder(), "plugins" + File.separator + NAME);
+
+    public DynamicPlugin() {
+        if (!FOLDER.exists()) {
+            FOLDER.mkdirs();
+        }
+    }
 
     public abstract void onLoad();
 
     public String getName() {
         return NAME;
+    }
+
+    public File getDataFolder() {
+        return FOLDER;
     }
 
 }
